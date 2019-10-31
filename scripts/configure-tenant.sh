13754e6e33defe12d602e3371b53e84fc0e70adb
Christian Heckelmann <christian.heckelmann@ert.com>
2019-10-31 10:21:09 +0100

    Adding dt_configure_tenant

---
+++ scripts/configure-tenant.sh
@@ -0,0 +1,38 @@
+#!/bin/env bash
+
+# Configure Dynatrace Tenant
+echo Configuring tenant $DT_TENANT_ID
+
+API="https://${DT_TENANT_ID}.live.dynatrace.com/api/config/v1/autoTags"
+
+# Check if rule already exists...
+tgrp=$(curl -s -H "Authorization: Api-Token ${DT_API_TOKEN}" -X GET "${API}" | jq '.values[] | select(.name == "deployment_group")')
+if [ -z "$tgrp" ]; then
+    echo "Create process_group rule."
+    curl -s -H "Authorization: Api-Token ${DT_API_TOKEN}" \
+      -H 'Content-Type: application/json' \
+      -X POST ${API} \
+      -d @deployment_group.json
+else
+    echo "Rule process_group exists."
+fi
+
+tgrp=$(curl -s -H "Authorization: Api-Token ${DT_API_TOKEN}" -X GET "${API}" | jq '.values[] | select(.name == "app")')
+if [ -z "$tgrp" ]; then
+    curl -s -H "Authorization: Api-Token ${DT_API_TOKEN}" \
+      -H 'Content-Type: application/json' \
+      -X POST ${API} \
+      -d @app.json
+else
+    echo "Rule environment exists."
+fi
+
+tgrp=$(curl -s -H "Authorization: Api-Token ${DT_API_TOKEN}" -X GET "${API}" | jq '.values[] | select(.name == "environment")')
+if [ -z "$tgrp" ]; then
+    curl -s -H "Authorization: Api-Token ${DT_API_TOKEN}" \
+      -H 'Content-Type: application/json' \
+      -X POST ${API} \
+      -d @environment.json
+else
+    echo "Rule app exists."
+fi
