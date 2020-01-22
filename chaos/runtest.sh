419e8c97e6d76751198acaf036015fe3039f9b6d
Christian Heckelmann <christian.heckelmann@ert.com>
2020-01-22 11:31:49 +0100

    Adding Node Drain Test

--- chaos/runtest.sh
+++ chaos/runtest.sh
@@ -2,11 +2,18 @@
 
 echo "Running tests for ${APPLICATION_SHORT_NAME} in ${CI_ENVIRONMENT_SLUG}..."
 
-# Prepare Chaosengine
-envsubst < chaosengine.yaml > chaosengine-${APPLICATION_SHORT_NAME}.yaml
-
 # Annotate deployment for chaos
 kubectl annotate deploy/${APPLICATION_SHORT_NAME} litmuschaos.io/chaos="true" -n ${CI_ENVIRONMENT_SLUG}
+
+# Get a worker node to run drain node test
+export NODENAME=$(kubectl get pods -n test -l app.kubernetes.io/name=${APPLICATION_SHORT_NAME} -o jsonpath='{.items[0].spec.nodeName}')
+
+echo "Selected node for Drain-Test: ${NODENAME}"
+# Cordon node, so litmus will not run his containers on this node
+kubectl cordon ${NODENAME}
+
+# Prepare Chaosengine
+envsubst < chaosengine.yaml > chaosengine-${APPLICATION_SHORT_NAME}.yaml
 
 # Create Resources
 kubectl create -f rbac.yaml -n ${CI_ENVIRONMENT_SLUG}
@@ -23,10 +30,10 @@ do
     if [ "$status" == "Completed" ]; then
         fin="1"
     else
-        echo "Chaos tests still running (${cnt}/100)..."
+        echo "Chaos tests still running (${cnt}/200)..."
         sleep 5
     fi
-    if [ "$cnt" == "100" ]; then
+    if [ "$cnt" == "200" ]; then
         echo "Chaos tests timed out!"
         exit 1
     fi
@@ -36,6 +43,10 @@ done
 kubectl describe chaosresult engine-${APPLICATION_SHORT_NAME}-container-kill -n ${CI_ENVIRONMENT_SLUG} > result-container-kill.tmp
 kubectl describe chaosresult engine-${APPLICATION_SHORT_NAME}-pod-network-loss -n ${CI_ENVIRONMENT_SLUG} > result-pod-network-loss.tmp
 kubectl describe chaosresult engine-${APPLICATION_SHORT_NAME}-pod-cpu-hog -n ${CI_ENVIRONMENT_SLUG} > result-pod-cpu-hog.tmp
+kubectl describe chaosresult engine-${APPLICATION_SHORT_NAME}-node-drain -n ${CI_ENVIRONMENT_SLUG} > result-node-drain.tmp
+
+# Uncordon node
+kubectl uncordon ${NODENAME}
 
 # Clean up
 kubectl delete -f chaosengine-${APPLICATION_SHORT_NAME}.yaml -n ${CI_ENVIRONMENT_SLUG}
