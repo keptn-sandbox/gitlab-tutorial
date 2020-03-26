25fc54fe96a80416b5c9e3407c034e47c5e8e2e1
Christian Heckelmann <christian.heckelmann@ert.com>
2020-03-26 08:53:12 +0100

    Adjust Load Test

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Search struct {
+	Text string
+}
 
 func ping(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "{\"ping\": \"OK\"}")
@@ -21,9 +26,23 @@ func error(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("500 - Something bad happened!"))
 }
 
+func search(w http.ResponseWriter, r *http.Request) {
+	var s Search
+
+	err := json.NewDecoder(r.Body).Decode(&s)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("500 - Something bad happened!"))
+		return
+	}
+	o := "Search string: " + s.Text
+	w.Write([]byte(o))
+}
+
 func handleRequests() {
 	http.HandleFunc("/service/ping", ping)
 	http.HandleFunc("/error", error)
+	http.HandleFunc("/search", error)
 	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
